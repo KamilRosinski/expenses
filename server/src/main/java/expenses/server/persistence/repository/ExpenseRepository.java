3ea18e1ffd739177e082603a47d8f6351740ff26
Kamil Rosinski <kamilrosiczka@gmail.com>
2020-04-14 00:36:52 +0200

    Clean code

--- server/src/main/java/expenses/server/persistence/repository/ExpenseRepository.java
+++ server/src/main/java/expenses/server/persistence/repository/ExpenseRepository.java
@@ -10,16 +10,18 @@ import java.util.Map;
 public interface ExpenseRepository extends CrudRepository<ExpenseEntity, Long> {
 
 	@Query("SELECT NEW MAP(" +
-				"YEAR(ee.date) AS year, " +
-				"MONTH(ee.date) AS month, " +
-				"SUM(CASE WHEN ee.value > 0 THEN ee.value ELSE 0 END) AS income, " +
-				"SUM(CASE WHEN ee.value < 0 THEN ee.value ELSE 0 END) AS outcome) " +
-			"FROM ExpenseEntity AS ee " +
+				"YEAR(expense.date) AS year, " +
+				"MONTH(expense.date) AS month, " +
+				"SUM(CASE WHEN expense.value > 0 THEN expense.value ELSE 0 END) AS income, " +
+				"SUM(CASE WHEN expense.value < 0 THEN expense.value ELSE 0 END) AS outcome) " +
+			"FROM ExpenseEntity AS expense " +
 			"GROUP BY year, month " +
 			"ORDER BY year, month DESC")
 	List<Map<String, Object>> getIncomeAndOutcomeGroupedByMonth();
 
-	@Query("FROM ExpenseEntity AS ee WHERE YEAR(ee.date) = :year AND MONTH(ee.date) = :month ORDER BY ee.date DESC")
-	List<ExpenseEntity> getExpensesByMonth(final int year, final int month);
+	@Query("FROM ExpenseEntity AS expense " +
+			"WHERE YEAR(expense.date) = :year AND MONTH(expense.date) = :month " +
+			"ORDER BY expense.date DESC")
+	List<ExpenseEntity> getExpensesByMonth(int year, int month);
 
 }
