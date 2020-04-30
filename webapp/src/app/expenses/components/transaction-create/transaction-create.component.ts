f8856d54bcf01914891783cd6fcba42b022530cb
Kamil Rosinski <kamilrosiczka@gmail.com>
2020-04-30 21:48:28 +0200

    Simple select added

--- webapp/src/app/expenses/components/transaction-create/transaction-create.component.ts
+++ webapp/src/app/expenses/components/transaction-create/transaction-create.component.ts
@@ -2,6 +2,7 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {ExpensesService} from '../../services/expenses.service';
 import {Observable} from 'rxjs';
 import {CategoryWithSubcategories} from '../../shared/category-with-subcategories';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
     selector: 'app-transaction-create',
@@ -10,19 +11,32 @@ import {CategoryWithSubcategories} from '../../shared/category-with-subcategorie
 })
 export class TransactionCreateComponent implements OnInit {
 
+    form: FormGroup;
     categories$: Observable<CategoryWithSubcategories[]>;
 
     @Output() cancel: EventEmitter<void> = new EventEmitter<void>();
 
-    constructor(private readonly expensesService: ExpensesService) {
+    get categoryInput(): AbstractControl {
+        return this.form.get('category');
+    }
+
+    constructor(private readonly expensesService: ExpensesService,
+                private readonly formBuilder: FormBuilder) {
+    }
+
+    ngOnInit(): void {
+        this.categories$ = this.expensesService.getCategoriesWithSubcategories();
+        this.form = this.formBuilder.group({
+            category: ['', [Validators.required]]
+        });
     }
 
     onCancel(): void {
         this.cancel.emit();
     }
 
-    ngOnInit(): void {
-        this.categories$ = this.expensesService.getCategoriesWithSubcategories();
+    onSubmit(): void {
+        console.log(this.form.value);
     }
 
 }
