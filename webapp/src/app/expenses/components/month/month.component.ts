52c7e63a1977cc62728a354f5b13e8ecdebb6a1b
Kamil Rosinski <kamilrosiczka@gmail.com>
2020-07-17 22:56:51 +0200

    Exception handling added to month component

--- webapp/src/app/expenses/components/month/month.component.ts
+++ webapp/src/app/expenses/components/month/month.component.ts
@@ -6,6 +6,8 @@ import {concatMap, map} from 'rxjs/operators';
 import {Observable} from 'rxjs';
 import {Transaction} from '../../shared/transaction';
 import {Prediction} from '../../shared/prediction';
+import {NotificationsService} from '../../../notifications/services/notifications.service';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
     selector: 'app-month',
@@ -18,16 +20,18 @@ export class MonthComponent implements OnInit {
     currentTab$: Observable<string>;
 
     constructor(private readonly expensesService: ExpensesService,
-                private readonly activatedRoute: ActivatedRoute) {
+                private readonly activatedRoute: ActivatedRoute,
+                private readonly notificationsService: NotificationsService) {
     }
 
     ngOnInit(): void {
 
         this.activatedRoute.paramMap.pipe(
             concatMap((params: ParamMap) => this.expensesService.getMonthById(+params.get('id')))
-        ).subscribe(
-            (month: Month) => this.month = month
-        );
+        ).subscribe({
+            next: (month: Month) => this.month = month,
+            error: (error: HttpErrorResponse) => this.notificationsService.show(`Failed to load expenses. Following error occurred: ${error.message}.`)
+        });
 
         this.currentTab$ = this.activatedRoute.queryParamMap.pipe(
             map((params: ParamMap) => params.get('tab'))
@@ -35,21 +39,24 @@ export class MonthComponent implements OnInit {
     }
 
     createTransaction(transaction: Transaction): void {
-        this.expensesService.createTransaction(this.month.id, transaction).subscribe(
-            (transaction: Transaction) => this.month.transactions = [...this.month.transactions, transaction]
-        );
+        this.expensesService.createTransaction(this.month.id, transaction).subscribe({
+            next: (transaction: Transaction) => this.month.transactions = [...this.month.transactions, transaction],
+            error: (error: HttpErrorResponse) => this.notificationsService.show(`Failed to create transaction. Following error occurred: ${error.message}.`)
+        });
     }
 
     deleteTransaction(transactionId: number): void {
-        this.expensesService.deleteTransaction(transactionId).subscribe(() => {
-            this.month.transactions = this.month.transactions.filter((transaction: Transaction) => transaction.id !== transactionId);
+        this.expensesService.deleteTransaction(transactionId).subscribe({
+            next: () => this.month.transactions = this.month.transactions.filter((transaction: Transaction) => transaction.id !== transactionId),
+            error: (error: HttpErrorResponse) => this.notificationsService.show(`Failed to delete transaction. Following error occurred: ${error.message}.`)
         });
     }
 
     createPrediction(prediction: Prediction): void {
-        this.expensesService.createPrediction(this.month.id, prediction).subscribe(
-            (prediction: Prediction) => this.month.predictions = [...this.month.predictions, prediction]
-        );
+        this.expensesService.createPrediction(this.month.id, prediction).subscribe({
+            next: (prediction: Prediction) => this.month.predictions = [...this.month.predictions, prediction],
+            error: (error: HttpErrorResponse) => this.notificationsService.show(`Failed to create prediction. Following error occurred: ${error.message}.`)
+        });
     }
 
 }
